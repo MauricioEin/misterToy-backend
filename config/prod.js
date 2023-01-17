3eb44824a853af53d6133fd2d9f718fa62794588
MauricioEin <mauricio.ein@gmail.com>
2023-01-17 12:59:08 +0200

    dotenv

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
-process.env.USER_NAME
-process.env.USER_KEY
+const username = process.env.USER_NAME
+const key = process.env.USER_KEY
 
 
 module.exports = {
-  'dbURL': `mongodb+srv://${USER_NAME}:${USER_KEY}@cluster0.oij4ow0.mongodb.net/?retryWrites=true&w=majority`,
+  'dbURL': `mongodb+srv://${username}:${key}@cluster0.oij4ow0.mongodb.net/?retryWrites=true&w=majority`,
 }
